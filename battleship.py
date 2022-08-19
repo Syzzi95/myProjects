8a14a099ad704b96f5dc5b80e09da80164275dda
sshahyy <sshahyy@studetn.jyu.fi>
2022-08-19 14:14:15 +0300

    some small modifications

--- battleship.py
+++ battleship.py
@@ -7,7 +7,7 @@ for x in range(5):
 
 def print_board(board):
   for row in board:
-    print " ".join(row)
+    print (" ".join(row))
 
 print_board(board)
 
@@ -28,25 +28,26 @@ ship_col = random_col(board)
 # Be sure to indent four spaces!
 def start_the_game(turns):
   for turn in range(turns): 
-    guess_row = int(raw_input("Guess Row: "))
-    guess_col = int(raw_input("Guess Col: "))
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
 
     if guess_row == ship_row and guess_col == ship_col:
-      print "Congratulations! You sunk my battleship!"
+      print ("Congratulations! You sunk my battleship!")
       break
     else:
-      if turn == turns: 
-        print "Game Over"
-      elif (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-        print "Oops, that's not even in the ocean."
-        print "Turn", (turn + 1)
+      if turn == turns - 1: 
+        print ("Game Over")
+        break
+      elif (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
+        print ("Oops, that's not even in the ocean.")
+        print ("Turn", (turn + 1))
       elif(board[guess_row-1][guess_col-1] == "X"):
-        print "You guessed that one already."
-        print "Turn", (turn + 1)
+        print ("You guessed that one already.")
+        print ("Turn", (turn + 1))
       else:
-        print "You missed my battleship!"
+        print ("You missed my battleship!")
         board[guess_row-1][guess_col-1] = "X"
-        print "Turn", (turn + 1) 
+        print ("Turn", (turn + 1)) 
         print_board(board)
   
 start_the_game(10)
